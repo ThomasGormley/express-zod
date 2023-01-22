4ee1643587d4217727b208bb0ca500ab21146a9a
Thomas Gormley <thomas.gormley@firstup.io>
2023-01-22 15:41:33 +0000

    init

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+/** @type {import('@types/eslint').Linter.BaseConfig} */
+module.exports = {
+  env: {
+    node: true,
+    es2021: true,
+  },
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  overrides: [],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint"],
+  rules: {},
+};
